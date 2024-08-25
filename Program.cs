32ca7a9a8cc2ea57901828c09f8c9ab0c8c7ea6a
v1rushb <cs.bashar.herbawi@gmail.com>
2024-08-25 21:19:16 +0300

    Product Class

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace InventoryApplication {
+    class Program {
+        internal static void Main(string[] args) {
+            Console.WriteLine("yoink");
+        }  
+    }
+
+}
